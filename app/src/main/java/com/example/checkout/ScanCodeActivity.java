b21cea2e474f09d097bc1c6473acd076f032d134
blnm <45343407+blnm@users.noreply.github.com>
2019-02-02 23:27:50 -0500

    barcode now recognized products

--- app/src/main/java/com/example/checkout/ScanCodeActivity.java
+++ app/src/main/java/com/example/checkout/ScanCodeActivity.java
@@ -9,6 +9,8 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
 public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
 
+    static String prod_name = "";
+    static double price     = 0;
 
     ZXingScannerView ScannerView;
 
@@ -21,8 +23,13 @@ public class ScanCodeActivity extends AppCompatActivity implements ZXingScannerV
 
     @Override
     public void handleResult(Result result) {
+        // scan result in result.getText
+        GetBarcodeDetail barcode = new GetBarcodeDetail();
+        barcode.barcode = result.getText();
+        barcode.execute();
 
-        MainActivity.resultTextView.setText(result.getText());
+        MainActivity.resultTextView.setText(prod_name + " " + price);
+
         onBackPressed();
 
     }
